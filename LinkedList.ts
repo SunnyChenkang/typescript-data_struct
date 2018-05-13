54cb7e4eabc2fe1f46bd1fdbde77bd0ba3ee78ad
SunnyChenkang <chenkangapple@126.com>
2018-05-13 22:26:16 +0800

    完整链表

--- LinkedList.ts
+++ LinkedList.ts
@@ -27,9 +27,97 @@ export class LinkedList
         this.length = 0;
     }
 
+    // 向链表尾部添加元素
     public Push( el : any )
     {
-        let node = new DataNode( el );
+        let node = new DataNode( el ) , current;
+        // 列表中第一个节点
+        if ( this.head == null ) this.head = node;
+        else 
+        {
+            current = this.head;
+            while( current.next ) current = current.next;
+            current.next = node;
+        }
+        this.length++;
+    }
+
+    // 移除链表指定位置
+    public RemoveAt( pos : number )
+    {   
+        if ( pos < 0 ) return null; 
+        if ( pos >= this.length ) return null;
+        let current = this.head , previous , index = 0;
+        
+        if ( pos === 0 ) this.head = current.next;
+        else
+        {
+            while ( index++ < pos ) 
+            {
+                previous = current;
+                current  = current.next;
+            }
+            previous.next = current.next;
+        }
+        this.length--;
+        return current.element;
+    }
+
+    // 删除指定元素
+    public Remove( el:any )
+    {
+        let index = this.IndexOf( el );
+        this.RemoveAt( index );
+    }
+
+    // 向链表中指定位置插入元素
+    public Insert( pos:number , el:any ) : boolean
+    {
+        if ( pos < 0 ) return false;
+        if ( pos >= this.length ) return false;
+        let node = new DataNode( el ) , current = this.head , previous , index = 0;
+        if ( pos === 0 )
+        {
+            node.next = current;
+            this.head = node;
+        }
+        else
+        {
+            while( index++ < pos )
+            {
+                previous  = current;
+                this.head = node;
+            }
+            node.next = current;
+            previous.next = node;
+        }
+        this.length++;
+        return true;
+    }
+
+    // 判断链表是否为空
+    public IsEmpty() : boolean
+    {
+        return this.length === 0;
+    }
+
+    // 返回链接长度
+    public Size() : number
+    {
+        return this.length;
+    }
+
+    // 查找元素位置
+    public IndexOf( el:any ) : number 
+    {
+        let current = this.head , index = 0;
+        while ( current ) 
+        {
+            if ( el === current ) { return index; }
+            index++;
+            current = current.next;
+        }
+        return -1;
     }
 }
 
